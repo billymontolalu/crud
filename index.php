608cdde056048e0bc2fe01f1d885afa130bad5bb
Billy Montolalu <billy.montolalu@gmail.com>
2022-06-27 10:39:25 +0700

    crud tabel berelasi

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ if ($conn->connect_error) {
 
 $sql = "select * from buku";
 $result = $conn->query($sql);
+
+$sql_peminjaman = "SELECT tanggal_pinjam, judul_buku, pengarang 
+FROM peminjaman LEFT JOIN buku ON peminjaman.idbuku = buku.id";
+
+$hasil_peminjaman = $conn->query($sql_peminjaman);
 ?>
 <html>
 
@@ -40,6 +45,31 @@ $result = $conn->query($sql);
                     <td><?php echo $i; ?></td>
                     <td><?php echo $row["judul_buku"]; ?></td>
                     <td><?php echo $row["pengarang"]; ?></td>
+                    <td><a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a> <a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
+                </tr>
+        <?php
+                $i = $i + 1;
+            }
+        }
+        ?>
+    </table>
+    <h1>Tabel Peminjaman</h1>
+    <table border="1">
+        <tr>
+            <td>No</td>
+            <td>Tanggal Pinjam</td>
+            <td>Judul Buku</td>
+            <td>Action</td>
+        </tr>
+        <?php
+        if ($hasil_peminjaman->num_rows > 0) {
+            $i = 1;
+            while ($row = $hasil_peminjaman->fetch_assoc()) {
+        ?>
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $row["tanggal_pinjam"]; ?></td>
+                    <td><?php echo $row["judul_buku"]; ?></td>
                     <td><a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a> <a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
                 </tr>
         <?php
